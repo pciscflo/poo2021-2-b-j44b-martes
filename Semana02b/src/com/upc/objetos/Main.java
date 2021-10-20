fdae514f5b3d8470ce1dc472369cbd5c3331073c
pciscflo <pciscflo@upc.edu.pe>
2021-10-19 22:06:38 -0500

    01.00

--- Semana02b/src/com/upc/objetos/Main.java
+++ Semana02b/src/com/upc/objetos/Main.java
@@ -1,9 +1,26 @@
 package com.upc.objetos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Alumno alumno = new Alumno("2021345A","Pedro",88, 88);
         System.out.println("Nombre:" + alumno.getNombre());
         System.out.println("Resultado:" + alumno.obtenerResultado());
+        List<Alumno> alumnos  = new ArrayList<>(); // Creando un arreglo de objetos vacio
+        alumnos.add(alumno);//adicionando un objeto al arreglo
+        Alumno alumno1 = new Alumno("2020134F", "Luis", 80, 80);
+        alumnos.add(alumno1);//adicionando un objeto al arreglo
+        Alumno alumno2 = new Alumno("2016789D", "Ana", 90, 90);
+        alumnos.add(alumno2);//adicionando un objeto al arreglo
+        System.out.println("**** Reporte *******");
+        for(Alumno p:alumnos){
+            System.out.println(p.getNombre()+ "  " + p.obtenerResultado());
+        }
+
+
+
+
     }
 }
