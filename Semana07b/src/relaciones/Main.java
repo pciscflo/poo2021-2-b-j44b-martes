5514185a2de1f027299d3b2243ea5784f7451a1d
pciscflo <pciscflo@upc.edu.pe>
2021-11-23 21:08:15 -0500

    03.00

--- Semana07b/src/relaciones/Main.java
+++ Semana07b/src/relaciones/Main.java
@@ -2,12 +2,12 @@ package relaciones;
 
 public class Main {
     public static void main(String[] args) {
-        Tanque tanque1 = new Tanque("Ruso1","Tubo34","Explot1");
-        Tanque tanque2 = new Tanque("Americano","Flash1","Balines");
-        Soldado soldado1 = new Soldado("Pepe", 17);
-        Soldado soldado2 = new Soldado("Juan", 19);
-        Soldado soldado3 = new Soldado("Jorge", 5);
-        Cuartel cuartel = new Cuartel("ARM100","Husares");
+        Atacante tanque1 = Factoria.obtenerObjeto("TA", "Ruso1", "Tubo34", "Explot1");
+        Atacante tanque2 = Factoria.obtenerObjeto("TA", "Americano", "Flash1", "Balines");
+        Atacante soldado1 = Factoria.obtenerObjeto("SO", "Pepe", "17");
+        Atacante soldado2 = Factoria.obtenerObjeto("SO", "Juan", "19");
+        Atacante soldado3 = Factoria.obtenerObjeto("SO", "Jorge", "5");
+        Cuartel cuartel = new Cuartel("ARM100", "Husares");
         try {
             cuartel.registrar(tanque1);
         } catch (Exception e) {
@@ -33,10 +33,10 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("Capacidad de Ataque:"+cuartel.obtenerCapacidadAtaque());
+        System.out.println("Capacidad de Ataque:" + cuartel.obtenerCapacidadAtaque());
         System.out.println("--------------****----------------");
-        for(Atacante p: cuartel.getArregloAtancantes()){
-            System.out.println(p.toString()+ "Atacando:" +  p.atacar());
+        for (Atacante p : cuartel.getArregloAtancantes()) {
+            System.out.println(p.toString() + "Atacando:" + p.atacar());
         }
     }
 }
